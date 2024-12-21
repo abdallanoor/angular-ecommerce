55783603c5ca17a9866857992721f213e6c91e18
Abdallah <abdallaahnoor@gmail.com>
2024-12-21 12:01:20 +0200

    feat: add Tailwind and PrimeNG

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,29 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { MyPreset } from './mytheme';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: MyPreset,
+        options: {
+          darkModeSelector: false,
+          // cssLayer: {
+          //   name: 'primeng',
+          //   order: 'tailwind-base, primeng, tailwind-utilities',
+          // },
+        },
+      },
+    }),
+  ],
 };
