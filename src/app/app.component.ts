21f16a6cc8500d8ae0fa1e832a0021e6ad524d54
Abdallah <abdallaahnoor@gmail.com>
2024-12-28 19:28:47 +0200

    add footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { MessageService } from 'primeng/api';
 import { ToastModule } from 'primeng/toast';
 import { ToastService } from './core/services/toast.service';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, ToastModule],
+  imports: [RouterOutlet, NavbarComponent, ToastModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   providers: [ToastService, MessageService],
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -5,8 +5,15 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
-
+  year: Number = new Date().getFullYear();
+  socialMediaIcons = [
+    { icon: 'pi pi-facebook' },
+    { icon: 'pi pi-instagram' },
+    { icon: 'pi pi-linkedin' },
+    { icon: 'pi pi-tiktok' },
+    { icon: 'pi pi-twitter' },
+  ];
 }
